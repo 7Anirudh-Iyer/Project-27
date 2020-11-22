04e782c43e1cb23bc06f8ed26130615af6ede849
Anirudh <anirudh.iyer@somaiya.edu>
2020-11-22 10:52:59 +0530

    I have commited this directory

---
+++ const.js
@@ -0,0 +1,15 @@
+class Const{
+    constructor(bodyA,bodyB){
+        var json={
+            bodyA: bodyA,
+            bodyB: bodyB,
+            length: 200,
+            stiffness: 2
+        }
+        this.body=Matter.Constraint.create(json)
+        World.add(world,this.body)
+    }
+    display(){
+
+    }
+}
